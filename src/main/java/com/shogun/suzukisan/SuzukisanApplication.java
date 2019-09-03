573d8f3df8b1bdab28d6ca0487e4e7d89c88add2
Piece123 <taka.rock.i4@gmail.com>
2019-09-03 11:37:45 +0900

    initial commit

---
+++ src/main/java/com/shogun/suzukisan/SuzukisanApplication.java
@@ -0,0 +1,13 @@
+package com.shogun.suzukisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuzukisanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuzukisanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shogun/suzukisan/SuzukisanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shogun.suzukisan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SuzukisanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
