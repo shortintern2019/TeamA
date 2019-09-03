73bca7780d98b431ae31932bb1bb28f109922b08
Piece123 <taka.rock.i4@gmail.com>
2019-09-03 14:16:15 +0900

    ブートストラップ

--- src/main/java/com/shogun/suzukisan/config/SecurityConfig.java
+++ src/main/java/com/shogun/suzukisan/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .antMatchers("/*.html", "/*.css")
-                .antMatchers("/bootstrap/**");
+                .antMatchers("/webjars/**");
     }
 
 }
