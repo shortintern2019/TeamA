c51b40660bbbe657bc6c7a9e80487ee9e07a93b1
TerukazuSuzuki <terukazusuzuki@keio.jp>
2019-09-05 11:33:30 +0900

    standbyのHTMLを作成

--- src/main/java/com/shogun/suzukisan/controller/ConversationController.java
+++ src/main/java/com/shogun/suzukisan/controller/ConversationController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ConversationController {
 
+    @GetMapping("/standby")
+    public String standby() {
+        return "standby";
+    }
+
     @GetMapping("/conversation")
     public String conversation() {
         return "conversation";
